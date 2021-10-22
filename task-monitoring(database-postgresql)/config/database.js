c381b5fa90e5339ca24f9d29d37e0c2584d816c9
Rizka <rizka.auliarahmi.tif419@polban.ac.id>
2021-10-22 19:16:14 +0700

    api update, add route

--- task-monitoring(database-postgresql)/config/database.js
+++ task-monitoring(database-postgresql)/config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const db = new Sequelize(
-    `postgresql://postgres:Titissamp22@localhost:5432/task-monitoring`,
+  `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DATABASE_PORT}/${process.env.DB_DATABASE}`,
     { define: { freezeTableName: true},
       logging : false}
 )
